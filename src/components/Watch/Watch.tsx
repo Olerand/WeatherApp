e5851097d3fef624acdaf66692f7a1e7134259ba
MikhailPotakovskiy <mihail.potakovskiy121@yandex.ru>
2025-04-01 11:30:16 +0300

    Finish

--- src/components/Watch/Watch.tsx
+++ src/components/Watch/Watch.tsx
@@ -22,37 +22,22 @@ interface WatchProps{
 const Watch:FC<WatchProps> = ({data}) => {
   const SunCalc = require('suncalc');
 
-
- const [goldenHourTime, setGoldenHourTime] = useState<string>('Loading...');
-
  const [morningGolden, setMorningGolden] = useState<string>('');
   const [eveningGolden, setEveningGolden] = useState<string>('');
 
   useEffect(() => {
     const fetchGoldenHours = async () => {
       try {
-        // 1. Получаем координаты города
         const response = await axios.get(
           `https://nominatim.openstreetmap.org/search?q=${data.location.name}&format=json`
         );
         const { lat, lon } = response.data[0];
-        
-        // 2. Парсим текущую дату
         const currentDate = new Date(data.location.localtime.replace(/-/g, '/'));
-
-        // 3. Рассчитываем время через SunCalc
         const { sunrise, sunset } = SunCalc.getTimes(currentDate, parseFloat(lat), parseFloat(lon));
-
-        // Утренний Golden Hour (1 час после восхода)
         const morningEnd = new Date(sunrise.getTime() + 3600000);
-        
-        // Вечерний Golden Hour (1 час до заката)
         const eveningStart = new Date(sunset.getTime() - 3600000);
-
-        // Форматируем время
         setMorningGolden(morningEnd.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
         setEveningGolden(eveningStart.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
-
       } catch (error) {
         console.error('Ошибка:', error);
         setMorningGolden('--:--');
