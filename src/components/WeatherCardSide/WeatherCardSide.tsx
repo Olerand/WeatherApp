5ed60ccd6507c36061fc4bfbea849d285bf79517
MikhailPotakovskiy <mihail.potakovskiy121@yandex.ru>
2025-04-08 21:37:40 +0300

    123

--- src/components/WeatherCardSide/WeatherCardSide.tsx
+++ src/components/WeatherCardSide/WeatherCardSide.tsx
@@ -1,5 +1,5 @@
 import React,{FC, useEffect, useState} from 'react'
-import cl from './WeatherCardSide.module.css'
+import cl from './styles.module.css'
 import { WeatherData } from '../../types'
 import InputSearch from '../InputSearch/InputSearch'
 import Watch from '../Watch/Watch'
