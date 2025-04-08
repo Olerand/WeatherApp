bb7e46a6e4eef37d1968264476bc31748533143e
MikhailPotakovskiy <mihail.potakovskiy121@yandex.ru>
2025-04-08 21:48:01 +0300

    123456

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const  App = () =>{
   const [city,setCity] = useState<string>(localStorage.lastCity || 'London')
   async function fetchWeather(city:string){
     try{
-      const response =  await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=5&aqi=yes&alerts=no`)
+      const response =  await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=5&aqi=yes&alerts=no`)
 
       setWeatherData(response.data)
     }catch (error){
